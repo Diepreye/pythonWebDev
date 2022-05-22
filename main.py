69e6326bb000348f5274292f1da3f373c64b0186
Diepreye <83272577+Diepreye@users.noreply.github.com>
2022-05-22 14:01:46 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 def calculator():
     print("This is a calculator to perform addition, subtraction, multiplication and division of two numbers")
     num_1 = int(input("enter the first number: "))
-    operand = input("enter the operation name or symbol, e.g 'addition or +': ")
+    operator = input("enter the operator name or symbol, e.g 'addition or +': ")
     num_2 = int(input("enter the second number: "))
 
-    if operand == "addition" or operand == "+":
+    if operator == "addition" or operator == "+":
         print(f"the sum of the two numbers is {num_1 + num_2}")
-    elif operand == "subtraction" or operand == "-":
+    elif operator == "subtraction" or operator == "-":
         print(f"the difference of the two numbers is {num_1 - num_2}")
-    elif operand == "multiplication" or operand == "*":
+    elif operator == "multiplication" or operator == "*":
         print(f"the product of the two numbers is {num_1 * num_2}")
-    elif operand == "division" or operand == "/":
+    elif opertor == "division" or operator == "/":
         print(f"the quotient of the two numbers is {num_1 / num_2}")
     else:
-        print("invalid operand")
+        print("invalid operator")
 
 
 calculator()
